b0aa42b1455682b3eb8d9c3fbcd6a3d4ed059769
unknown <danielly.figueiredo@aluno.senai.br>
2025-04-22 15:30:04 -0300

    Lista de exercicio 3- repetição

--- Soma dez números/somadez.js
+++ Soma dez números/somadez.js
@@ -1,13 +1,13 @@
 let somaNum = 0;
 for (let i = 1; i <= 10; i++) {
     
-    let numDez = parseInt(prompt(`Digite o ${i}° número para que seja efetuada a soma`));
+    let numDez = parseInt(prompt(`Digite o ${i}° número para que seja efetuada a soma`));// Solocita ao usuário um número para que seja efetuada a soma 
 
-    if (isNaN(numDez)) {
+    if (isNaN(numDez)) { // Verifica se o número inserido é válido
         alert("Erro: você não digitou um número válido");
 
     } else {
-        somaNum += numDez; 
+        somaNum += numDez; // Efetua a soma dos dez números inseridos pelo usuário
     }
 } 
-console.log (`A soma dos números é equivalente a: ${somaNum}`);
+console.log (`A soma dos números é equivalente a: ${somaNum}`); // Mostra o resultado da soma dos tres números
--- Tabuada Repetição/tabuada.js
+++ Tabuada Repetição/tabuada.js
@@ -4,7 +4,7 @@ if (isNaN(tabuadaNum)) {
     alert(" Erro: você não digitou um número válido");
 }
 
- for(let i = 1; i <= 10; i++ ){
+ for(let i = 1; i <= 10; i++ ){ // Verifica o número que o usuario inseriu e faz a tabuada dele
     alert (` ${tabuadaNum} x ${i} = ${tabuadaNum * i}`);
 }
     
